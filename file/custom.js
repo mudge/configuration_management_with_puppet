be1ee9b43fe4f72f09f40af59530b6ac1c0c7b66
Paul Mucur <mudge@mudge.name>
2011-04-17 20:49:22 +0100

    static presentation

--- file/custom.js
+++ file/custom.js
@@ -1,6 +1,8 @@
 /* Automatically transition to the next slide after 20 seconds. */
 (function nextSlide() {
   nextStep();
-  setTimeout(nextSlide, 20 * 1000);
+  if (slidenum !== slideTotal) {
+    setTimeout(nextSlide, 20 * 1000);
+  }
 }());
 
